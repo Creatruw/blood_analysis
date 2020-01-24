eb7064bb603e2254e146b0fb7f5854b1169061d2
Yuhao Xue <yx161@duke.edu>
2020-01-24 13:03:01 -0500

    add LDL_analysis funciton in chol_analysis

--- chol_analysis.py
+++ chol_analysis.py
@@ -5,7 +5,18 @@ def HDL_analysis(HDL_level):
         return "Borderline low"
     else:
         return "Low"
-
+        
+        
+def LDL_analysis(LDL_level):
+    if LDL_level <= 130:
+        return "normal"
+    elif 131 <= LDL_level <=159:
+        return "borderline high"
+    elif 160<= LDL_level <=189:
+        return "high"
+    else:
+        return "very high"
+        
 
 def cholesterol_analysis():
     print("cholesterol analysis")
@@ -14,6 +25,9 @@ def cholesterol_analysis():
     if test_info[0] == "HDL":
         answer = HDL_analysis(int(test_info[1]))
         print("the level of HDl is {}".format(answer))
+    elif test_info[0] == "LDL":
+        answer = LDL_analysis(int(test_info[1]))
+        print("the level of LDL is {}".format(answer))
 
 def interface():
     while True:
